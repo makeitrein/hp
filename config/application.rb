bd3165814c99a319b34385bc7adb2813fcd90e90
Jordan Rein <jrein@opuslogica.com>
2014-08-11 19:41:52 -0700

    guard init plus some additions to config generators

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,16 @@ module Hpfic
     # config.i18n.default_locale = :de
 
     # For Foundation 5
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       :fixtures => true,
+                       :view_specs => false,
+                       :helper_specs => false,
+                       :routing_specs => false,
+                       :controller_specs => true,
+                       :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories" end
     config.assets.precompile += %w( vendor/modernizr )
 
   end
