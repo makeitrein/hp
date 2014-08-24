f81d87bdda818d5d542d3ad0d196d426cb1c331f
Jordan Rein <jrein@opuslogica.com>
2014-08-23 18:24:17 -0700

    added username to user, added views for login and signup, styled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140824005232) do
+ActiveRecord::Schema.define(version: 20140824005824) do
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140824005232) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
